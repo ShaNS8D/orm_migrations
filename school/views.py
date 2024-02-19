5805a5e4c3da41b0791750ec150ff131721a42b7
Shalaevskiy Nikolay <nshalaevskiy@intraservices.ru>
2024-02-19 12:22:12 +0300

    first commit

---
+++ school/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+    students_list = Student.objects.all().order_by(ordering).prefetch_related('teachers')
+    context = {
+        'object_list': students_list,
+    }
+    return render(request, template, context)
